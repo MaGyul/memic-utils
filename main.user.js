2a6e338c1d30e00db71cf88dd618a18e617d36d2
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2025-08-02 11:19:39 +0900

    Add files via upload

---
+++ main.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         미밐 유저 스크립트 관리자
+// @namespace    https://page.magyul.kr/mimic-utils
+// @version      1.0.0
+// @description  미밐의 불편함을 유저 스크립트로 제작해서 관리하던걸 하나로 통합
+// @author       MaGyul
+// @author       isnoa
+// @match        *://memic.at/*
+// @match        *://shelter.id/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=memic.at
+// @require      https://raw.githubusercontent.com/MaGyul/memic-utils/main/memic-utils.js
+// @updateURL    https://raw.githubusercontent.com/MaGyul/memic-utils/main/main.user.js
+// @downloadURL  https://raw.githubusercontent.com/MaGyul/memic-utils/main/main.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
---
+++ memic-utils.js
@@ -0,0 +1,7 @@
+(() => {
+    class MemicUtils {
+        
+    }
+
+    window.memicUtils = new MemicUtils();
+})();
