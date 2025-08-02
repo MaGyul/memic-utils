8175a7f41f28d874be2e6454e98f1dab7ef5d2f4
MaGyul <magyul906@gmail.com>
2025-08-02 18:36:55 +0900

    전역 타입 정의 및 MemicUtils 클래스의 API 메서드 개선

--- global.d.ts
+++ global.d.ts
@@ -14,6 +14,146 @@
 // === global ===
 
 declare const localforage: import("./localforage").LocalForage;
+declare const memicUtils: MemicUtils;
+
+declare interface BoardInfo {
+    color: string;
+    description: string;
+    headers: string[];
+    icon: string;
+    id: number;
+    isAdultOnly: boolean;
+    link: string;
+    listItemType: string;
+    name: string;
+    newestArticleCreateDate: string;
+    order: number;
+    permission: any;
+    shelter: number;
+    type: string;
+    viewType: string;
+}
+
+declare interface ImageInfo {
+    contentType: string;
+    fileSize: number;
+    metadata: {
+        width: number;
+        height: number;
+    };
+    path: string;
+    signature: string;
+    url: string;
+}
+
+declare interface OwnerInfo {
+    id: string;
+    image: ImageInfo;
+    isCertificationCreator: boolean;
+    listItemType: string;
+    name: string;
+    personalId: string;
+}
+
+declare interface ShelterInfo {
+    createDate: string;
+    description: string;
+    background: ImageInfo;
+    highlightColor: string;
+    id: number;
+    isAttendanceEnabled: boolean;
+    isCertificationCreator: boolean;
+    isCommunityEnabled: boolean;
+    isOnlyAccessAdult: boolean;
+    listItemType: string;
+    name: string;
+    owner: OwnerInfo;
+    tags: string[];
+}
+
+declare interface ArticleInfo {
+    articleType: string;
+    board: BoardInfo;
+    commentCount: number;
+    commentType: string;
+    createDate: string;
+    header: string;
+    id: number;
+    isHaveImage: boolean;
+    isHaveVideo: boolean;
+    isLicked: boolean;
+    isMasked: boolean;
+    isNotice: boolean;
+    isOnlyAdult: boolean;
+    isRead: boolean;
+    isSecret: boolean;
+    lickCount: number;
+    listItemType: string;
+    owner: OwnerInfo;
+    shelter: ShelterInfo;
+    shelterId?: number;
+    thumbnail: ImageInfo;
+    title: string;
+    viewCount: number;
+}
+
+declare interface ArticleListResponse {
+    hasNext: boolean;
+    hasPrev: boolean;
+    list: ArticleInfo[];
+}
+
+declare class AddonStorage {
+    static getStorage(name: string): AddonStorage;
+    constructor(name: string);
+    get<T>(key: string, defaultValue: T): Promise<T>;
+    set<T>(key: string, value: T): Promise<void>;
+}
+
+declare class Logger {
+    static getLogger(name: string): Logger;
+    constructor(name: string);
+    log(...args: any[]): void;
+    info(...args: any[]): void;
+    warn(...args: any[]): void;
+    error(...args: any[]): void;
+}
+
+declare class MemicAPI {
+    getAuthToken(): string;
+    request(endpoint: string, options?: RequestInit): Promise<any>;
+
+    articles: {
+        getList(shelterId: string, isNotice?: boolean, size?: number, offsetId?: string): Promise<ArticleListResponse>;
+        getListByBoard(boardId: string, isNotice?: boolean, size?: number, offsetId?: string): Promise<ArticleListResponse>;
+        get(articleId: string): Promise<ArticleInfo>;
+    }
+
+    shelters: {
+        get(shelterId: string): Promise<ShelterInfo>;
+        search(keyword: string, size?: number, offsetId?: string): Promise<ShelterInfo>;
+        getBoards(shelterId: string): Promise<BoardInfo[]>;
+    }
+
+    personal: {
+        getShelter(personalId: string): Promise<ShelterInfo>;
+        getShelterBoards(shelterId: string): Promise<BoardInfo[]>;
+    }
+}
+
+declare class MemicUtils {
+    #systemStorage: AddonStorage;
+    #enabledAddons: string;
+    #errorAddons: { addonName: string, err: Error }[];
+
+    api: MemicAPI;
+    uiAddon: Addon;
+    addons: Record<string, Addon>;
+    enabledAddons: string[];
+    logger: Logger;
+
+    get errorAddons(): { addonName: string, err: Error }[];
+}
 
 declare interface Addon {
     addonKey: string;
--- memic-utils.js
+++ memic-utils.js
@@ -227,74 +227,29 @@
         // 📝 게시글 관련 API
         articles = {
             // 게시글 목록 조회
-            getList: async (params = {}) => {
+            getList: async (shelterId, isNotice, size, offsetId) => {
                 const searchParams = new URLSearchParams();
                 
-                if (params.keyword) searchParams.append('keyword', params.keyword);
-                if (params.shelterId) searchParams.append('shelterId', params.shelterId);
-                if (params.ownerId) searchParams.append('ownerId', params.ownerId);
-                if (params.searchType) searchParams.append('searchType', params.searchType);
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
+                if (isNotice) searchParams.append('isNotice', isNotice);
+                if (size) searchParams.append('size', size);
+                if (offsetId) searchParams.append('offsetId', offsetId);
 
-                return this.request(`/articles?${searchParams}`);
+                return this.request(`/shelters/${shelterId}/articles?${searchParams}`);
+            },
+
+            getListByBoard: async (boardId, isNotice, size, offsetId) => {
+                const searchParams = new URLSearchParams();
+
+                if (isNotice) searchParams.append('isNotice', isNotice);
+                if (size) searchParams.append('size', size);
+                if (offsetId) searchParams.append('offsetId', offsetId);
+
+                return this.request(`/shelters/-/boards/${boardId}/articles?${searchParams}`);
             },
 
             // 게시글 상세 조회
             get: async (articleId) => {
                 return this.request(`/articles/${articleId}`);
-            },
-
-            // 공개 게시글 목록
-            getPublic: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/articles/public?${searchParams}`);
-            },
-
-            // 게시글 ID들로 일괄 조회
-            getByIds: async (articleIds) => {
-                return this.request('/articles/ids', {
-                    method: 'POST',
-                    body: JSON.stringify({ articleIds })
-                });
-            },
-
-            // 쉘터에 게시글 작성
-            createInShelter: async (shelterId, data) => {
-                return this.request(`/shelters/${shelterId}/articles`, {
-                    method: 'POST',
-                    body: JSON.stringify(data)
-                });
-            },
-
-            // 보드에 게시글 작성
-            createInBoard: async (boardId, data) => {
-                return this.request(`/boards/${boardId}/articles`, {
-                    method: 'POST',
-                    body: JSON.stringify(data)
-                });
-            },
-
-            // 게시글 수정
-            update: async (articleId, data) => {
-                return this.request(`/articles/${articleId}`, {
-                    method: 'PUT',
-                    body: JSON.stringify(data)
-                });
-            },
-
-            // 게시글 검색
-            search: async (keyword, params = {}) => {
-                const searchParams = new URLSearchParams();
-                searchParams.append('keyword', keyword);
-                
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/articles?${searchParams}`);
             }
         };
 
@@ -306,11 +261,11 @@
             },
 
             // 쉘터 검색
-            search: async (keyword, params = {}) => {
+            search: async (keyword, size, offsetId) => {
                 const searchParams = new URLSearchParams();
                 if (keyword) searchParams.append('keyword', keyword);
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
+                if (offsetId) searchParams.append('offsetId', offsetId);
+                if (size) searchParams.append('size', size);
 
                 return this.request(`/shelters/search?${searchParams}`);
             },
@@ -318,200 +273,6 @@
             // 쉘터 보드 목록
             getBoards: async (shelterId) => {
                 return this.request(`/shelters/${shelterId}/boards`);
-            }
-        };
-
-        // 📋 보드 관련 API
-        boards = {
-            // 보드별 게시글 목록
-            getArticles: async (boardId, params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.keyword) searchParams.append('keyword', params.keyword);
-                if (params.searchType) searchParams.append('searchType', params.searchType);
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/boards/${boardId}/articles?${searchParams}`);
-            }
-        };
-
-        // 👤 사용자 관련 API
-        users = {
-            // personalId로 사용자 조회
-            getByPersonalId: async (personalId) => {
-                return this.request(`/users/personal/${personalId}`);
-            }
-        };
-
-        // 🔐 내 정보 관련 API
-        me = {
-            // 내 게시글 목록
-            getArticles: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.keyword) searchParams.append('keyword', params.keyword);
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/me/articles?${searchParams}`);
-            },
-
-            // 내 댓글 목록
-            getComments: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/me/comments?${searchParams}`);
-            },
-
-            // 내 타임라인
-            getTimeline: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/me/timeline?${searchParams}`);
-            },
-
-            // 내 기본 쉘터
-            getBaseShelter: async () => {
-                return this.request('/me/base-shelter');
-            },
-
-            // 가입한 쉘터 목록
-            getShelters: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/me/shelters/settlers/newest-date?${searchParams}`);
-            },
-
-            // 즐겨찾기 쉘터 목록
-            getFavoriteShelters: async (shelterIds = []) => {
-                const searchParams = new URLSearchParams();
-                shelterIds.forEach(id => searchParams.append('shelterIds[]', id));
-                searchParams.append('pageRequest.size', shelterIds.length || 20);
-
-                return this.request(`/me/shelters/-/favorites?${searchParams}`);
-            }
-        };
-
-        // 💾 임시글 관련 API
-        tempArticles = {
-            // 임시글 목록
-            getList: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.shelter) searchParams.append('shelter', params.shelter);
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/me/temp/articles?${searchParams}`);
-            },
-
-            // 임시글 조회
-            get: async (tempArticleId) => {
-                return this.request(`/me/temp/articles/${tempArticleId}`);
-            },
-
-            // 임시글 저장
-            create: async (data) => {
-                return this.request('/me/temp/articles', {
-                    method: 'POST',
-                    body: JSON.stringify(data)
-                });
-            },
-
-            // 임시글 수정
-            update: async (tempArticleId, data) => {
-                return this.request(`/me/temp/articles/${tempArticleId}`, {
-                    method: 'PUT',
-                    body: JSON.stringify(data)
-                });
-            },
-
-            // 임시글 삭제
-            delete: async (tempArticleId) => {
-                return this.request(`/me/temp/articles/${tempArticleId}`, {
-                    method: 'DELETE'
-                });
-            }
-        };
-
-        // ❤️ 좋아요 관련 API
-        likes = {
-            // 좋아요 추가
-            add: async (likeType, parentId) => {
-                return this.request('/likes', {
-                    method: 'POST',
-                    body: JSON.stringify({ likeType, parentId })
-                });
-            },
-
-            // 좋아요 취소
-            remove: async (likeType, parentId) => {
-                const searchParams = new URLSearchParams();
-                searchParams.append('likeType', likeType);
-                searchParams.append('parentId', parentId);
-
-                return this.request(`/likes?${searchParams}`, {
-                    method: 'DELETE'
-                });
-            }
-        };
-
-        // 🏆 랭킹 관련 API
-        rankings = {
-            // 통합 메타쉘터 랭킹
-            getMetaShelters: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/rankings/consolidate/meta-shelters?${searchParams}`);
-            },
-
-            // 월 후원액 랭킹
-            getMonthlySponsor: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/rankings/monthly-sponsor?${searchParams}`);
-            },
-
-            // 추천 랭킹
-            getRecommend: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/rankings/recommend?${searchParams}`);
-            }
-        };
-
-        // 🎯 리더보드 관련 API
-        leaderboards = {
-            // 인기 쉘터 랭킹
-            getShelters: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/leaderboards/shelters?${searchParams}`);
-            }
-        };
-
-        // 🖥️ UI 관련 API
-        ui = {
-            // 메인 페이지 데이터
-            getMain: async () => {
-                return this.request('/ui/main');
-            },
-
-            // 로그인 사용자 메인 데이터
-            getMeMain: async () => {
-                return this.request('/ui/me/main');
             }
         };
 
@@ -525,28 +286,6 @@
             // 개인의 쉘터 보드 목록
             getShelterBoards: async (personalId) => {
                 return this.request(`/personal/${personalId}/shelter/boards`);
-            }
-        };
-
-        // 🎯 퀵필터 관련 API
-        quickFilters = {
-            // 쉘터 클릭 기록
-            setLatestClick: async (shelterId) => {
-                return this.request(`/me/quick-filters/-/shelters/${shelterId}:click`, {
-                    method: 'PATCH'
-                });
-            }
-        };
-
-        // 🔍 메타쉘터 필드 관련 API
-        metaShelterFields = {
-            // 메타쉘터 필드 목록
-            getList: async (params = {}) => {
-                const searchParams = new URLSearchParams();
-                if (params.offsetId) searchParams.append('pageRequest.offsetId', params.offsetId);
-                if (params.size) searchParams.append('pageRequest.size', params.size);
-
-                return this.request(`/meta-shelters/fields?${searchParams}`);
             }
         };
     }
